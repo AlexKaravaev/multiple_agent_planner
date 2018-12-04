f0310a412187202941048876d53880e8beb8e964
AlexKaravaev <AlexKaravev@gmail.com>
2018-12-04 22:37:19 +0300

    Structure is completely reworked

--- multi_robot_sim/scripts/sim_start.py
+++ multi_robot_sim/scripts/sim_start.py
@@ -5,13 +5,13 @@ import os
 if __name__=="__main__":
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     _dir = os.path.dirname(__file__)
-    
+
     os.system("rosnode kill /robosim")
     os.system("rosnode kill /map_server")
     os.system("rosnode kill /rviz")
     os.system("rosnode kill /stageros")
 
-    os.system("gnome-terminal -- roslaunch detached_planner robot_diff_drive_in_stage.launch")
+    os.system("gnome-terminal -- roslaunch multi_robot_sim robot_diff_drive_in_stage.launch")
     #launch = roslaunch.parent.ROSLaunchParent(uuid,
     #            [(_dir[:-len('scripts')] + 'launch/robot_diff_drive_in_stage.launch')])
 
